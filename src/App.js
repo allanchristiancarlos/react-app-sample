49b6b2943ebebdad4759e5a1de67dbbe2f4323a5
Allan Christian Carlos <allanchristiancarlos@gmail.com>
2019-06-15 12:56:53 +0800

    sample rxjs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jss</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  inputChanged$ = new Subject();
+  componentDidMount() {
+    this.inputChanged$
+      .asObservable()
+      .pipe(debounceTime(300))
+      .subscribe(x => {
+        console.log(x);
+      });
+  }
+  handleInputChange = e => {
+    this.inputChanged$.next(e.target.value);
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <input type="text" onChange={this.handleInputChange} />
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.jss</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
